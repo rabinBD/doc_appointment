27404c73e208d0ecf117f37379cd6409f13582fe
rabindarlami <rabindrabin36@gmail.com>
2024-09-14 08:07:10 +0545

    added to server

--- server/config/config.js
+++ server/config/config.js
@@ -6,20 +6,25 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT
   },
   test: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT
+
   },
   production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT
+
   }
 };
